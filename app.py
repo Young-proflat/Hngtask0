7b6dcedf2c0f1919de7e81f6f85d5f27a2213337
Abdullateef <olatunbosunlateef6@gmail.com>
2025-02-01 10:54:06 +0100

    commit

--- app.py
+++ app.py
@@ -20,13 +20,19 @@ timezone = pytz.timezone("Africa/Lagos")
 
 @app.get("/")
 
+
+timezone = pytz.timezone("Africa/Lagos")
+
+@app.get("/")
 def mygetapi():
     email = "olatunbosunlateef6@gmail.com"
-    current_datetime=(datetime.now()).isoformat()+ "Z"
-    github_url ="https://github.com/Young-proflat/Hngtask0/"
+    current_datetime = datetime.now(timezone).isoformat() + "Z"
+    github_url = "https://github.com/Young-proflat/Hngtask0/"
+    
     response = {
         "email": email,
-        "current_datetime" : current_datetime,
-        "github_url": github_url,
+        "current_datetime": current_datetime,
+        "github_url": github_url
     }
+    
     return response
